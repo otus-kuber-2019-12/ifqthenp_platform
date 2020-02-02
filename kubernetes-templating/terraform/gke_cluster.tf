3f871521cc2a39ec648035862ab4188e89e02374
Andrei Bogomja <bogomja@gmail.com>
2020-02-02 06:42:32 +0000

    Changes nodes count to 1

--- kubernetes-templating/terraform/gke_cluster.tf
+++ kubernetes-templating/terraform/gke_cluster.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "gke-cluster" {
   name               = "otus-hw-kubernetes-templating"
   network            = "default"
-  initial_node_count = 2
+  initial_node_count = 1
   location           = "europe-west2-a"
   description        = "Kubernetes Templating HW"
 }
