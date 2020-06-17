5abc5851abf4bf64aedd4718d717b8aae0af5ef7
Andrei Bogomja <bogomja@gmail.com>
2020-06-17 16:18:32 +0100

    Adds data for GKE version and outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,29 @@ terraform {
   }
 }
 
+data "google_container_engine_versions" "euwest2a" {
+  location       = var.cluster_zone
+  version_prefix = "1.16."
+}
+
 resource "google_container_cluster" "gke-cluster" {
   name               = var.cluster_name
   network            = "default"
   initial_node_count = var.node_count
   location           = var.cluster_zone
   description        = var.cluster_description
+  node_version       = data.google_container_engine_versions.euwest2a.latest_node_version
+  min_master_version = data.google_container_engine_versions.euwest2a.latest_master_version
+}
+
+output "latest_node_version" {
+  value = data.google_container_engine_versions.euwest2a.latest_node_version
+}
+
+output "latest_master_version" {
+  value = data.google_container_engine_versions.euwest2a.latest_master_version
+}
+
+output "default_cluster_version" {
+  value = data.google_container_engine_versions.euwest2a.default_cluster_version
 }
